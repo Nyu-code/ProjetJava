7dc21432d36394a34522bddd02f7a86c344a8133
sir <66173239+tomx93@users.noreply.github.com>
2020-05-30 19:33:38 +0200

    Add files via upload

--- Personnage.java
+++ Personnage.java
@@ -1,34 +1,39 @@
+import java.util.ArrayList;
+
 public abstract class Personnage {
-	private static String [] tabBlessure = {"en forme","superficielle","blessé","gravement blessé","inconscient","mort"}; //blessure de 0 à 6
-	private static String [] tabPotion = {"molotov","soin","explosion","mana"};
-	private static String inventaire[] = new String[15];
-	private String blessure;
-	private int exp;	//experience
-	private int init;	//initiative
-	private int atk;	//attaque
-	private int esq;	//esquive
-	private int def;	//defense
-	private int dgt;	//degat
+	protected double hp;
+	protected double maxHp = 100;
+	protected static String[] malus = {"poison","atkDeb","esqDeb"};
+	//initDeb provient d'une potion de molotov
+	//esqDeb provient d'une potion d'explosion
+	protected ArrayList<Item> inventaire = new ArrayList<Item>(16);
+	protected ArrayList<String> etat = new ArrayList<String>();
+	protected static final int TAILLE_ARRAYLIST = 16;
+	protected String blessure;
+	protected Item gauche,droite; //ce qu'il y a dans la main gauche et ce qu'il y a dans la main droite
+	protected int exp,init,atk,esq,def,dgt;
 	
 	public Personnage() {
-		for( int i = 0; i < 15; i++ )
-			  inventaire[i] = "";
-		this.blessure = tabBlessure[0];
+		this.hp = maxHp;
+		this.setBlessure();
+		for (int i = 0; i<this.inventaire.size();i++) {
+			this.inventaire.set(i,null);
+		}
 		this.exp = 0;
 		this.init = 0;
 		this.atk = 0;
 		this.esq = 0;
 		this.def = 0;
 		this.dgt = 0;
-		 
-		
 	}
 	
-	public Personnage(int blessure, int exp, int init, int atk, int esquive, int defense, int degat) {
+	public Personnage(int exp, int init, int atk, int esquive, int defense, int degat) {
 		super();
-		for( int i = 0; i < 15; i++ )
-			  inventaire[i] = "";
-		this.blessure = tabBlessure[blessure];
+		this.hp = maxHp;
+		this.setBlessure();
+		for (int i = 0; i<this.inventaire.size();i++) {
+			this.inventaire.set(i,null);
+		}
 		this.exp = exp;
 		this.init = init;
 		this.atk = atk;
@@ -37,32 +42,13 @@ public abstract class Personnage {
 		this.dgt = degat;
 	}
 
-	public Personnage(Personnage p) {
-		for( int i = 0; i < 15; i++ )
-			  inventaire[i] = "";		
-		this.blessure = p.blessure;
-		this.exp = p.exp;
-		this.init = p.init;
-		this.atk = p.atk;
-		this.esq = p.esq;
-		this.def = p.def;
-		this.dgt = p.dgt;
-	}
 
-	public static String[] getTabBlessure() {
-		return tabBlessure;
-	}
-
-	public static String[] getTabPotion() {
-		return tabPotion;
-	}
-
-	public String[] getInventaire() {
-		return inventaire;
+	public ArrayList<Item> getInventaire() {
+		return this.inventaire;
 	}
 
 	public String getBlessure() {
-		return blessure;
+		return this.blessure;
 	}
 
 	public int getExp() {
@@ -89,20 +75,67 @@ public abstract class Personnage {
 		return dgt;
 	}
 
-	public static void setTabBlessure(String[] tabBlessure) {
-		Personnage.tabBlessure = tabBlessure;
+	public double getHp() {
+		return hp;
 	}
 
-	public static void setTabPotion(String[] tabPotion) {
-		Personnage.tabPotion = tabPotion;
+	public double getMaxHp() {
+		return maxHp;
 	}
 
-	public static void setInventaire(String[] inventaire) {
-		Personnage.inventaire = inventaire;
+	public void setHp(double hp) {
+		if (hp>maxHp && hp<0) {
+			new Exception("On ne peut pas modifier son HP");
+		} else { 	
+			this.hp = hp;
+			this.setBlessure();
+		}
 	}
 
-	public void setBlessure(String blessure) {
-		this.blessure = blessure;
+	public void setMaxHp(float maxHp) {
+		this.maxHp = maxHp;
+	}
+
+	public void setInventaire(ArrayList<Item> inventaire) {
+		this.inventaire = inventaire;
+	}
+	
+	
+	
+	public void setBlessure() { //méthode qui applique automatiquement sa blessure relative au nombre d'HP du joueur
+//		{"en forme","superficielle","légèrement","blessé","gravement blessé","inconscient","mort"}
+		double pourcentageHP = (this.hp/maxHp)*100;
+		double palier = maxHp/7;
+		int nbPalier = (int) (pourcentageHP/palier);
+		if (hp == 0) {
+			this.blessure="mort";
+		} else {
+			switch (nbPalier){
+				case 0 :
+					this.blessure="inconscient" ;
+					break;
+				case 1 :
+					this.blessure="gravement blessé" ;
+					break;
+				case 2 :
+					this.blessure="gravement blessé" ;
+					break;
+				case 3 :
+					this.blessure="blessé" ;
+					break;
+				case 4 :
+					this.blessure="légèrement blessé" ;
+					break;
+				case 5 :
+					this.blessure="superficielle" ;
+					break;
+				case 6 :
+					this.blessure="en forme" ;
+					break;
+				default:
+					this.blessure="en pleine forme";
+			}
+		}
 	}
 
 	public void setExp(int exp) {
@@ -141,38 +174,99 @@ public abstract class Personnage {
 	public int dee() {	//méthode pour un dée de 6 faces
 		return rand(1,6);
 	}
-	
-	public void afficheInventaire() {
-        System.out.println("Inventaire : ");
-        for (int i = 0; i<inventaire.length ; i++ ) {
-            System.out.print(inventaire[i]+", ");
-        }
-        System.out.println("");
-    }
 
-    public boolean addInventaire(String objet) {
-    	int i = 0;
-    	while(!inventaire[i].equals("")){
-    	  i++;
-    	}
-    	if (i == (inventaire.length)-1) {
-    		System.out.println("L'inventaire est plein !");
-    		return false;
-    	}
-    	else {
-    	inventaire[i] = objet;
-    	return true;
-    	}
-    }
-	public boolean removeInventaire(String objet) {
-		for(int i = 0; i<inventaire.length;i++) {
-			if(inventaire[i]==objet) {
-				inventaire[i] = "";
+	
+	public boolean estDansInventaire(Item p) {
+		for (	int i = 0	; i<(inventaire.size())	;	i++) {
+			if (p.contentEquals(inventaire.get(i))) {
 				return true;
 			}
 		}
 		return false;
 	}
+	
+	public void afficheInventaire() {
+		System.out.println("Inventaire "+"("+inventaire.size()+")"+": ");
+		String s = "";
+		if (inventaire.size()==0) {
+			s = "vide";
+		}
+		else {
+			for (int i = 0; i<inventaire.size()-1 &&inventaire.get(i)!=null ; i++ ) {
+	            s += i+1 + ". " + inventaire.get(i).toString()+", ";
+	        }
+			s += inventaire.size() + ". " + inventaire.get(inventaire.size()-1).toString()+".";
+		}
+        System.out.println(s);
+        System.out.println("");
+    }
+
+    public void addInventaire(Item objet) {
+    	
+    	if (inventaire.size()==16) {
+    		System.out.println("Inventaire plein");
+    	}
+    	
+    	else {
+    		int i = 0;
+            while(i<(inventaire.size()) && !inventaire.get(i).equals(null)) {
+              i++;
+            }
+            inventaire.add(objet);
+    	}
+    }
+    public void removeIndInventaire(int indice) {
+    	inventaire.remove(indice);
+    }
+	
+	public void removeInventaire(Item item) {
+        for(int i = 0; i<inventaire.size();i++) {
+            if(inventaire.get(i)==item) {
+                inventaire.remove(i);
+                break;
+            }
+        }
+    }
+	
+	public int getNbPot() { //méthode pour compter le nombre de potion dans l'inventaire
+		int compteur = 0;
+		for (int i  = 0; i< inventaire.size();i++) {
+			if (inventaire.get(i) instanceof Potion) {
+				compteur++;
+			}
+		}
+		return compteur;
+	}
+	
+	public void affichePotion() {
+		String s = "";
+		int nb = this.getNbPot();
+		for (int i = 0; i < inventaire.size()-1;i++) {
+			if (inventaire.get(i) instanceof Potion) {
+				s += (i+1) +" "+ inventaire.get(i).toString() + ",";
+			}
+		}
+		
+		if (inventaire.get(inventaire.size()-1) instanceof Potion) {
+			s += inventaire.get(inventaire.size()-1).toString() + ".";
+		}
+		System.out.println("Potion"+"("+nb+")");
+		System.out.println(s);
+	}
+	
+	
+//	abstract void deplacer(); //pour plus tard !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//	
+//	abstract void attaquer();
+//	
+//	abstract void ramasser(String obj);
+//	
+//	abstract void deposer(String obj);
+//	
+//	abstract String affichePA();
+//	
+//	abstract void changePA(int point);
+
 	
 	
 
