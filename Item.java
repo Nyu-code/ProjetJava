eb32051d4e2abff705db0608c79743f7b894ae0c
sir <66173239+tomx93@users.noreply.github.com>
2020-05-30 20:17:18 +0200

    Add files via upload

--- Item.java
+++ Item.java
@@ -1,12 +1,20 @@
 public class Item {
-    public String categorie;            //potion ou arme ou protection
-    
-    public Item() {
-        this.categorie = "";
-    }
-    
-    public Item(String item) {
-        this.categorie = item;
-    }
-    
+	public String categorie;			//potion ou arme ou protection
+	
+	public Item() {
+		this.categorie = "";
+	}
+	
+	public Item(String item) {
+		this.categorie = item;
+	}
+
+	public boolean contentEquals(Item item) {
+		return this.equals(item);
+	}
+	
+	public String toString() {
+		return this.categorie;
+	}
+	
 }
